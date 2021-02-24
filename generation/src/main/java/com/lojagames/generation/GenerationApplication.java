a9616eb5401f66b3a8e1d032da6f67a811fc74f7
Carolina Guida <carolinaguidaa@gmail.com>
2021-02-24 13:50:21 -0300

    feat(API): Created API with SpringBoot

---
+++ generation/src/main/java/com/lojagames/generation/GenerationApplication.java
@@ -0,0 +1,13 @@
+package com.lojagames.generation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerationApplication.class, args);
+	}
+
+}
