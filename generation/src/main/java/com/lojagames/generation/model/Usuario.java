942db423ff371022133bc97657c79ad98adf0e78
Carolina Guida <carolinaguidaa@gmail.com>
2021-02-25 15:55:34 -0300

    feat(model): Add table usuario

---
+++ generation/src/main/java/com/lojagames/generation/model/Usuario.java
@@ -0,0 +1,65 @@
+package com.lojagames.generation.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull
+	@Size(min = 2, max = 50)
+	private String nomeUsuario;
+
+	@NotNull
+	@Email
+	private String email;
+
+	@NotNull
+	@Size(min = 8, max = 20)
+	private String senha;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
