4e4b746407190e0a5058e3b176288561bf2b00fe
Carolina Guida <carolinaguidaa@gmail.com>
2021-03-01 14:29:13 -0300

    fix(model): remove attributes from table usuario

--- generation/src/main/java/com/lojagames/generation/model/Usuario.java
+++ generation/src/main/java/com/lojagames/generation/model/Usuario.java
@@ -1,19 +1,14 @@
 package com.lojagames.generation.model;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
+
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
 @Entity
@@ -35,10 +30,10 @@ public class Usuario {
 	@NotNull
 	@Size(min = 8, max = 20)
 	private String senha;
-	
+
 	/*
-	@ManyToMany
-	private List<Produto> produto;*/
+	 * @ManyToMany private List<Produto> produto;
+	 */
 
 	public long getId() {
 		return id;
@@ -72,12 +67,9 @@ public class Usuario {
 		this.senha = senha;
 	}
 	/*
-	public List<Produto> getProduto() {
-		return produto;
-	}
-
-	public void setProduto(List<Produto> produto) {
-		this.produto = produto;
-	}*/
+	 * public List<Produto> getProduto() { return produto; }
+	 * 
+	 * public void setProduto(List<Produto> produto) { this.produto = produto; }
+	 */
 
 }
